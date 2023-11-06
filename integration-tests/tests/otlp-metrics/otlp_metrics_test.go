329ff938b50a677f0c7ad787469ac9981d8d4069
Paschalis Tsilias <paschalis.tsilias@grafana.com>
2023-11-06 13:44:53 +0200

    Disable integration test
    Signed-off-by: Paschalis Tsilias <paschalis.tsilias@grafana.com>

--- integration-tests/tests/otlp-metrics/otlp_metrics_test.go
+++ integration-tests/tests/otlp-metrics/otlp_metrics_test.go
@@ -13,11 +13,16 @@ func TestOtlpMetrics(t *testing.T) {
 	var metricResponse common.MetricResponse
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		err := common.FetchDataFromURL(query, &metricResponse)
-		assert.NoError(c, err)
-		if assert.NotEmpty(c, metricResponse.Data.Result) {
-			assert.Equal(c, metricResponse.Data.Result[0].Metric.Name, "span_metrics_duration_bucket")
-			assert.Equal(c, metricResponse.Data.Result[0].Metric.TestName, "otlp_metrics")
-			assert.NotEmpty(c, metricResponse.Data.Result[0].Value.Value)
-		}
+		//assert.NoError(c, err)
+		//if assert.NotEmpty(c, metricResponse.Data.Result) {
+		// 	assert.NoError(c, err)
+		//	assert.Equal(c, metricResponse.Data.Result[0].Metric.Name, "span_metrics_duration_bucket")
+		//	assert.Equal(c, metricResponse.Data.Result[0].Metric.TestName, "otlp_metrics")
+		//		assert.NotEmpty(c, metricResponse.Data.Result[0].Value.Value)
+		//}
+		// Disabling the above checks as part of the workaround in #5684.
+		// TODO(@ptodev, @tpaschalis) Fix this once the workaround in #5684 is removed.
+		assert.Error(c, err)
+		assert.Empty(c, metricResponse.Data.Result)
 	}, common.DefaultTimeout, common.DefaultRetryInterval, "Data did not satisfy the conditions within the time limit")
 }
